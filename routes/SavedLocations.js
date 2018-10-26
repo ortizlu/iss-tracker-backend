a0dcffe206909a1eb87c30d814498feb94fde640
Luis Ortiz <ortizlu@icloud.com>
2018-10-25 20:44:14 -0400

    fix

--- routes/SavedLocations.js
+++ routes/SavedLocations.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const SavedLocations = require('../models/savedLocationModel');
-const User = require('../models/userModel.js');
-const jwtDecode = require('jwt-decode');
+const express = require('express')
+const router = express.Router()
+const SavedLocations = require('../models/savedLocationModel')
+const User = require('../models/userModel.js')
+const jwtDecode = require('jwt-decode')
 
 // GET ALL LOCATIONS
 router.get('/', (req, res) => {
@@ -17,47 +17,48 @@ router.post('/', (req, res) => {
     SavedLocations.create(req.body)
       .then(location => {
         res.json(location)
-        founduser.savedLocations.push(location)})
+        founduser.savedLocations.push(location)
+      })
       .then(_ => founduser.save(err => console.log(err)))
-      .catch(err => console.log(err));
-  });
-});
+      .catch(err => console.log(err))
+  })
+})
 
 //FIND ONE AND SHOW
 router.get('/:id', (req, res) => {
   User.findById(jwtDecode(req.headers.authorization).id).then(founduser => {
-  SavedLocations.findOne({ _id: req.params.id })
-    .then(location => {
-      res.json(location);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  });
-});
+    SavedLocations.findOne({ _id: req.params.id })
+      .then(location => {
+        res.json(location)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
+})
 
 //FIND ONE AND UPDATE
 router.put('/:id', (req, res) => {
-  User.findById(jwtDecode(req.headers.authorization).id).then(founduser => {
+  // User.findById(jwtDecode(req.headers.authorization).id).then(founduser => {
   SavedLocations.findOneAndUpdate({ _id: req.params.id }, req.body)
     .then(updatedLocation => {
-      res.json(updatedLocation);
+      res.json(updatedLocation)
     })
     .catch(err => {
-      console.log(err);
-    });
-  });
-});
+      console.log(err)
+    })
+  // });
+})
 
 //FIND ONE AND DELETE
 router.delete('/:id', (req, res) => {
   SavedLocations.findOneAndDelete({ _id: req.params.id })
     .then(deletedLocation => {
-      res.json(deletedLocation);
+      res.json(deletedLocation)
     })
     .catch(err => {
-      console.log(err);
-    });
-});
+      console.log(err)
+    })
+})
 
-module.exports = router;
+module.exports = router
